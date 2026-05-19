76f1759a21ec42135acd321006e035e5fbf0331c
Vershitel18 <Ognezvezdg@gmail.com>
2026-05-19 22:14:02 +0300

    push_back_big_shared_fixed

--- src/socow-vector.h
+++ src/socow-vector.h
@@ -419,15 +419,9 @@ public:
           clear();
           swap(tmp);
         } else {
-          SocowVector tmp(new_capacity(size()));
+          SocowVector tmp(*this, new_capacity(size()));
           new (tmp.big_->data_ + size()) T(std::forward<U>(value));
-          try {
-            std::uninitialized_copy_n(raw_data(), size(), tmp.raw_data());
-          } catch (...) {
-            (tmp.big_->data_ + size())->~T();
-            throw;
-          }
-          tmp.size_ = size() + 1;
+          ++tmp.size_;
           clear();
           swap(tmp);
         }
